e3a645f9b841b52f64eb8c94d856a8a77ca67bed
Michael Gold <goldmichael@gmail.com>
2023-10-16 13:04:07 -0400

    fix: load model animations

--- components/CharacterScene.tsx
+++ components/CharacterScene.tsx
@@ -1,35 +1,26 @@
-import { Canvas } from '@react-three/fiber';
-import { Suspense, useRef, useEffect } from 'react';
-import { useGLTF, useAnimations } from '@react-three/drei';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas, useThree } from "@react-three/fiber";
+import { Suspense, useRef, useEffect } from "react";
+import { useGLTF, useAnimations, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 const Character: React.FC = () => {
-  const ref = useRef();
-  const { nodes, animations } = useGLTF('/mstr-walking-firefighter.glb');
-  const { actions } = useAnimations(animations, ref);
+  const { scene, animations } = useGLTF("/mstr-walking-firefighter.glb");
+  const group = useRef();
+  const { actions, mixer } = useAnimations(animations, group); // might need mixer for more complex animations
+
+  useThree(({ camera }) => {
+    camera.position.z = 2;
+    camera.lookAt(0, 0, 0);
+  }); // set camera position for default zoom
 
   useEffect(() => {
     if (actions && actions.animation) {
       actions.animation.play();
-      console.log(nodes.hips);
-      console.log(animations[0].tracks);
-      console.log(actions.animation._clip.tracks[1]);
-      console.log(Object.keys(nodes));
-
     }
   }, [actions]);
 
-  return (
-    <group ref={ref} scale={[2, 2, 2]}>
-      <mesh 
-        geometry={nodes.mesh.geometry} 
-        material={nodes.mesh.material} 
-        position={[0, 0, 0]} 
-      />
-    </group>
-  );
+  return <primitive ref={group} object={scene} dispose={null} />;
 };
-
 const CharacterScene: React.FC = () => {
   return (
     <Canvas>
